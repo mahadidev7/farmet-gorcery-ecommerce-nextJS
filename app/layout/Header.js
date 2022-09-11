b953b94335d8d3952341a01e0eae3439b0446eb0
mahadidev7 <mahadidev7@gmail.com>
2022-09-11 14:24:52 +0600

    setup stacture

---
+++ app/layout/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-white'>
+        <div className="container mx-auto">
+          Hello
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ pages/products/index.js
@@ -0,0 +1,8 @@
+
+const Products = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Products
